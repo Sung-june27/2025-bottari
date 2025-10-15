92340a3ba2e2a88c1e1aba312026ad694564dce7
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-10-15 13:47:42 +0900

    [BE] 개발, 운영 서버 DB Write/Read 분리 (#582)
    * chore: 개발 서버 Mysql master, replica db 설정 추가
    
    * feat: 로컬 dataSource 설정 추가
    
    * feat: dev, prod 읽기/쓰기 dataSource 분리
    
    * refactor: aop 방식 제거, 트랜잭션 메니저를 통한 read/write 분리
    
    * feat: readOnly 트랜잭션 설정 추가
    
    * refactor: 코드 컨벤션 정리
    
    * feat: 분리된 master/replica Hikari 데이터소스 설정 추가
    
    * chore: dev, prod ci write/read 데이터베이스 분리
    
    * chore: replica db 설정 주석 제거
    
    * refactor: 코드 컨벤션 정리

--- backend/bottari/src/main/java/com/bottari/bottaritemplate/service/BottariTemplateService.java
+++ backend/bottari/src/main/java/com/bottari/bottaritemplate/service/BottariTemplateService.java
@@ -45,6 +45,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BottariTemplateService {
 
